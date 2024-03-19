aacc69475c3814a0b3f39a2ed324b53dc0cb1251
singhTannu-827 <singhtannu@gmail.com>
2024-03-20 01:51:54 +0530

    install telwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header underline">
+        <h1>React DOM</h1>
+        <h1 className="text-3xl font-bold underline text-zinc-200 bg-lime-600">
+          Hello world!
+        </h1>
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+  Button
+</button>
       </header>
+      
     </div>
+  
   );
 }
 
